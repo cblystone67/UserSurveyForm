47dfbf2be39a0c78e9a8c8d17d3b5cc56058d76f
cblystone67 <christopherblystone1@gmail.com>
2024-02-16 18:11:52 -0800

    added python code for turtle import

--- mypython.py
+++ mypython.py
@@ -1,10 +1,26 @@
+import random
+import turtle
+
+t = turtle.Turtle()
+
+
+t.forward(50)
+
+turtle.done()
+
+
 print("Hello World")
 
 x = "awesome"
 
 
 def myfunc():
+    x = "fantastic"
     print("Python is " + x)
 
 
 myfunc()
+print("Python is " + x)
+print(type(x))
+
+print(random.randrange(1, 10))
